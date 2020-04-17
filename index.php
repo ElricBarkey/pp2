7c3169feb4bad456b91314eafc54c393f78f14c4
Elric Barkey <ebarkey2@mail.greenriver.edu>
2020-04-17 16:21:58 -0400

    Add index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    /*
+     * Elric Barkey
+     * 4/17/2020
+     * http://ebarkey.greenriverdev.com/328/pp2/index.php
+     * Pair program 2
+     */
+
+    // Turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+?>
+
+<?php
+
+    echo "PHP Array Practice";
+
